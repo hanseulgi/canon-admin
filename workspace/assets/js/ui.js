64eb183b19e1b899d498dc6d6d39522e24dd0df6
hanseulgi <hanseulgi90@gmail.com>
2022-01-12 21:27:47 +0900

    [한슬기] 작업중

--- workspace/assets/js/ui.js
+++ workspace/assets/js/ui.js
@@ -8,6 +8,21 @@ UI.window = $(window);
 UI.doc = $(document);
 UI.body = $('body');
 
+// dim
+UI.dim = function () {
+
+    // 해당 data 속성이 있고, 마우스 댔을때 이벤트 실행
+    const $targetDim = $('[data-dim]');
+
+    $targetDim.off("mouseover").on("mouseover", function() {
+        $('.dim').addClass('on');
+    });
+
+    $targetDim.off("mouseleave").on("mouseleave", function() {
+        $('.dim').removeClass('on');
+    });
+
+};
 
 // 생성자
 UI.a = function() {};
@@ -21,12 +36,12 @@ UI.c = (function () {
         init: function () { //이걸 실행
             this.open();   //여기서 this는 UI임
         },
-        
+
         open: function ( target ) {
 
         },
         close: function ( target ) {
-            
+
         }
     }
 })();  //즉시 실행
@@ -39,8 +54,8 @@ UI.variable = 0;
 UI.modules = {};
 
 // 객체 컨테이너 안에 객체 사용 가능
-UI.modules.module1 = {}; 
-UI.modules.module1.data = { a : 1, b : 2 }; 
+UI.modules.module1 = {};
+UI.modules.module1.data = { a : 1, b : 2 };
 UI.modules.module2 = {};
 
 
@@ -50,9 +65,10 @@ UI.init = function(){
 	UI.a();
 	UI.b();
 	UI.c.init();
+    UI.dim();
 };
 
 /* 실행 */
 (function(){
-   UI.init();
+    UI.init();
 })();
