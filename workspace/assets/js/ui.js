967ba262bb553667886ad60241a5749d24a87862
hanseulgi <hanseulgi90@gmail.com>
2022-01-17 17:46:32 +0900

    [한슬기] 일단 commit

--- workspace/assets/js/ui.js
+++ workspace/assets/js/ui.js
@@ -1,17 +1,41 @@
 var UI = UI || {};
 
 // if(typeof UI === 'undefined') {  위에꺼랑 결국 같은 뜻
-// 	var UI = {}; 
-// } 
+// 	var UI = {};
+// }
 
 UI.window = $(window);
 UI.doc = $(document);
 UI.body = $('body');
 
+
+
+// 생성자
+// UI.a = function() {};
+// UI.b = function() {};
+
+// UI.c = (function () {
+//     const $var1 = '';
+//     let _var2 = '';
+
+//     return {
+//         init: function () { //이걸 실행
+//             this.open();   //여기서 this는 UI임
+//         },
+
+//         open: function ( target ) {
+
+//         },
+//         close: function ( target ) {
+
+//         }
+//     }
+// })();  //즉시 실행
+
+
 // dim
 UI.dim = function () {
 
-    // 해당 data 속성이 있고, 마우스 댔을때 이벤트 실행
     const $targetDim = $('[data-dim]');
 
     $targetDim.off("mouseover").on("mouseover", function() {
@@ -30,59 +54,35 @@ UI.navToggle = function () {
     const $target = $el.next('.nav');
 
     $el.off("click").on("click", function() {
+        $(this).toggleClass('on');
         $target.toggleClass('on');
     });
 
-    // $el.off("").on("", function() {
-    //     $('').removeClass('');
-    // });
-
 };
 
+UI.dropDown = function () {
 
-// 생성자
-UI.a = function() {};
-UI.b = function() {};
+    $('.nav-menu > li > a').off("click").on("click", function(e) {
 
-UI.c = (function () {
-    const $var1 = '';
-    let _var2 = '';
+        e.preventDefault();
 
-    return {
-        init: function () { //이걸 실행
-            this.open();   //여기서 this는 UI임
-        },
+        $('.sub-menu').slideUp();
+        $(this).next().slideDown();
 
-        open: function ( target ) {
+    });
 
-        },
-        close: function ( target ) {
-
-        }
-    }
-})();  //즉시 실행
-
-
-// 변수
-UI.variable = 0;
-
-// 객체 컨테이너
-UI.modules = {};
-
-// 객체 컨테이너 안에 객체 사용 가능
-UI.modules.module1 = {};
-UI.modules.module1.data = { a : 1, b : 2 };
-UI.modules.module2 = {};
+};
 
 
 
 /* 실행 선언 */
 UI.init = function(){
-	UI.a();
-	UI.b();
-	UI.c.init();
+	// UI.a();
+	// UI.b();
+	// UI.c.init();
     UI.dim();
     UI.navToggle();
+    UI.dropDown();
 };
 
 /* 실행 */
