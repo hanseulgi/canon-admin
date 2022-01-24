69bbc13f31caa30d04bc1d4fb3d93a8323d26231
hanseulgi <hanseulgi90@gmail.com>
2022-01-24 15:57:11 +0900

    [한슬기] dim삭제

--- workspace/assets/js/ui.js
+++ workspace/assets/js/ui.js
@@ -34,7 +34,7 @@ UI.body = $('body');
 
 
 
-/* popup */
+/* modal */
 let open = function(el) {
     $(el).fadeIn();
 };
@@ -57,6 +57,7 @@ let close = function(el) {
 //     });
 
 // };
+
 
 /* file remove */
 UI.file  = function () {
@@ -188,20 +189,20 @@ UI.datepicker = function () {
        ,showOtherMonths: true //빈 공간에 현재월의 앞뒤월의 날짜를 표시
        ,showMonthAfterYear:true //년도 먼저 나오고, 뒤에 월 표시
        // ,changeYear: true //콤보박스에서 년 선택 가능
-       // ,changeMonth: true //콤보박스에서 월 선택 가능                    
+       // ,changeMonth: true //콤보박스에서 월 선택 가능
        ,yearSuffix: "년" //달력의 년도 부분 뒤에 붙는 텍스트
        ,monthNamesShort: ['1','2','3','4','5','6','7','8','9','10','11','12'] //달력의 월 부분 텍스트
        ,monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'] //달력의 월 부분 Tooltip 텍스트
        ,dayNamesMin: ['일','월','화','수','목','금','토'] //달력의 요일 부분 텍스트
        ,dayNames: ['일요일','월요일','화요일','수요일','목요일','금요일','토요일'] //달력의 요일 부분 Tooltip 텍스트
        // ,minDate: "-1M" //최소 선택일자(-1D:하루전, -1M:한달전, -1Y:일년전)
-       // ,maxDate: "+1M" //최대 선택일자(+1D:하루후, -1M:한달후, -1Y:일년후)                    
-   });
+       // ,maxDate: "+1M" //최대 선택일자(+1D:하루후, -1M:한달후, -1Y:일년후)
+    });
 
-   $( ".datepicker" ).datepicker();
+    $( ".datepicker" ).datepicker();
 
-   /* skin */
-   $( ".datepicker" ).datepicker('widget').wrap('<div class="ll-skin-santiago"/>');
+    /* skin */
+    $( ".datepicker" ).datepicker('widget').wrap('<div class="ll-skin-santiago"/>');
 
 };
 
@@ -211,7 +212,7 @@ UI.init = function(){
 	// UI.a();
 	// UI.b();
 	// UI.c.init();
-    UI.dim();
+    // UI.dim();
     UI.navToggle();
     UI.dropDown();
     UI.search();
